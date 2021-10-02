f40eab6b515a80520a65dfab694f1a8bcc3fde02
Leonardo Carraro <lcarraro.oliveira@gmail.com>
2021-10-02 18:05:38 -0300

    initial commit

---
+++ real-meet-service/src/main/java/com/leoncarraro/realmeetservice/RealMeetServiceApplication.java
@@ -0,0 +1,12 @@
+package com.leoncarraro.realmeetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealMeetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealMeetServiceApplication.class, args);
+    }
+}
